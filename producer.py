60f0efb3b8d0235fdbc2ab0f24194f238561d7b0
Mohamed Omar Rouis <79800710+omarrouis@users.noreply.github.com>
2025-09-17 14:53:08 +0200

    Update producer.py

--- producer.py
+++ producer.py
@@ -5,7 +5,7 @@ from kafka import KafkaProducer
 # Create a Kafka producer instance
 # The value_serializer ensures the message is a JSON string
 producer = KafkaProducer(
-    bootstrap_servers='localhost:9092',
+    bootstrap_servers='kafka:29092',
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
 
